9aaa8452d27be658bb3f521c1a827a720d3ceb3f
PM-Apexx <125660663+PM-Apexx@users.noreply.github.com>
2024-03-15 17:37:22 +0000

    Update payment-test.js

--- Payment-demo/payment-test.js
+++ Payment-demo/payment-test.js
@@ -40,7 +40,7 @@ class ApiClient {
   }
 }
 // The rest of your code remains the same
-const apiKey = 'f742b7dcA75c6A406eAb1cbAf01be0047514';
+const apiKey = 'c6490381A6ab0A4b18A9960Af3a9182c40ba';
 const baseUrl = 'https://sandbox.apexx.global/atomic/v1/api/payment/hosted';
 const apiClient = new ApiClient(baseUrl, apiKey);
 let paymentInitiated = false;
