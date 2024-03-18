7d3a1cdf65f9de479d9a42359aeac2472cc93a57
PM-Apexx <125660663+PM-Apexx@users.noreply.github.com>
2024-03-18 14:37:03 +0000

    Update payment-test.js

--- Payment-demo/payment-test.js
+++ Payment-demo/payment-test.js
@@ -171,8 +171,8 @@ const initiateSofortPayment = (basket) => {
     });
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  const basket = [];
+//document.addEventListener('DOMContentLoaded', () => {
+  //const basket = [];
 
   document.querySelectorAll('.add-to-basket').forEach(button => {
     button.addEventListener('click', function() {
@@ -209,13 +209,13 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     console.error('Pay with Card button not found');
   }
-  const cartButton = document.getElementById('cart');
-  cartButton.addEventListener('click', () => {
-    if (basket.length > 0) {
-      displayPaymentForm();
-      initiatePayment(basket);
-    } else {
-      alert('Your basket is empty.');
-    }
+  //const cartButton = document.getElementById('cart');
+  //cartButton.addEventListener('click', () => {
+    //if (basket.length > 0) {
+     // displayPaymentForm();
+      //initiatePayment(basket);
+   // } else {
+     // alert('Your basket is empty.');
+    //}
   });
 });
