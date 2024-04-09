6fb9b828a580249535417400c570b6078c29956c
PM-Apexx <125660663+PM-Apexx@users.noreply.github.com>
2024-04-09 16:09:54 +0100

    Update payment-test.js

--- Payment-demo/payment-test.js
+++ Payment-demo/payment-test.js
@@ -124,7 +124,7 @@ const displayPaymentForm = () => {
   }
 };
 const initiateKlarnaPayment = async (basket) => {
-  const totalAmount = basket.reduce((total, item) => total + item.net_unit_price, 0);
+  //const totalAmount = basket.reduce((total, item) => total + item.net_unit_price, 0);
   const paymentData = {
     organisation: 'ff439f6eAc78dA4667Ab05aAc89f92e27f76',
     currency: 'GBP',
