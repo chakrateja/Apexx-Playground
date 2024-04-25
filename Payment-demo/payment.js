0091199ac9d0b78f4f38f5d64e65f0379820091d
PM-Apexx <125660663+PM-Apexx@users.noreply.github.com>
2024-04-25 14:39:34 +0100

    Update payment.js

--- Payment-demo/payment.js
+++ Payment-demo/payment.js
@@ -543,19 +543,8 @@ const initiateSezzlePayment = async () => {
     try {
       const responseData = await apiClient.sendRequest('', 'POST', paymentData, 'hosted');
       if (responseData && responseData.url) {
-        const paymentIframe = document.getElementById('payment-iframe');
-        if (paymentIframe) {
-          paymentIframe.onload = () => {
-            paymentIframe.style.display = 'block';
-          };
-          paymentIframe.src = responseData.url;
-        } else {
-          console.error('Payment iframe not found');
-        }
-        const paymentForm = document.getElementById('payment-form');
-        if (paymentForm) {
-          paymentForm.style.display = 'block';
-        }
+        // Open the payment form in a new window
+        window.open(responseData.url, '_blank');
         paymentInitiated = true;
       } else {
         showError('Failed to initiate payment');
